c1d867c4cbdc68bc6f91b844eb39d0c33dc32a27
leandro <malvesblox@gmail.com>
2026-02-27 08:37:17 -0300

    calculadora media

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-alert("Eu funcionei")
+const numero1 = document.getElementById("numero1")
+const numero2 = document.getElementById("numero2")
+const button = document.getElementById("button")
+
+function calculaMedia(){
+const nota1 = Number(numero1.value)
+const nota2 = Number(numero2.value)
+  const media = (nota1 + nota2)/2
+
+  console.log(media)
+
+}
+
+button.addEventListener("click", calculaMedia)
