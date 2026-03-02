861ebbfcaa6d3c43d4ad9202e0895557bb5fbc53
leandro <leandrosanches127@gmail.com>
2026-03-02 10:19:23 -0300

    leandro

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const numero1 = document.getElementById("numero1")
 const numero2 = document.getElementById("numero2")
 const button = document.getElementById("button")
+const resultado = document.getElementById("resultado")
 
 function calculaMedia(){
 const nota1 = Number(numero1.value)
 const nota2 = Number(numero2.value)
+
   const media = (nota1 + nota2)/2
 
-  console.log(media)
-
+  resultado.innerHTML = `A media é ${media}`
 }
 
 button.addEventListener("click", calculaMedia)
